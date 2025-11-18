e9f6e57037db2cc8aaf36f9f204acce4bfa52535
Sandro <Sandro.bonomo@gmx.de>
2025-11-18 23:57:08 +0100

    wichtige änderungen

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
 
 
-let dialogRefArray = ['first_dialog', 'second_dialog', 'third_dialog','fourth_dialog', 'fifth_dialog','sixth_dialog'];
-currentIndex = 0;
+let imgArray = ['./img/bay-9939855_1280.jpg','./img/faroe-islands-9939486_1280.jpg','./img/rose-9939147_1280.jpg','./img/flying-9940425_1280.jpg','./img/africa-9904004_1280.jpg','./img/poisonous-snake-9829897_1280.jpg']
 
+const container = document.getElementById('imgContainer');
 
-function closeAllDialogs() {
-    dialogRefArray.forEach(id => {
-        const dlg = document.getElementById(id);
-        if (dlg.open) dlg.close();
-    });
+for (let i = 0; i < imgArray.length; i++) {
+            const img = document.createElement('img');
+            img.src = imgArray[i];
+            container.appendChild(img);
 }
 
 
-function openDialog(id) {
-    currentIndex = dialogRefArray.indexOf(id);
-    closeAllDialogs();
-    const dlg = document.getElementById(id);
-    dlg.showModal();
-}
+const imgContainer = document.querySelector("#imgContainer");
+const dialog = document.querySelector("#dialog");
+const closeBtn = document.querySelector(".escBtn");
+
+
+imgContainer.addEventListener("click", (event, i) => {
+    if (event.target.tagName === "IMG") {
+        dialog.showModal();
+        const img = document.createElement('img');
+        img.src = imgArray[i];
+    }
+});
 
 
 
-
-let arrayLength = dialogRefArray.length ;
-
-
-function nextDialog(){
-    if (currentIndex < arrayLength -1) {
-        currentIndex++;
-        openDialog(dialogRefArray[currentIndex]);
-    }else openDialog(dialogRefArray[0])
-}
-
-
-function prevDialog() {
-    if (currentIndex > 0) {
-        currentIndex--;
-        openDialog(dialogRefArray[currentIndex]);
-    }else openDialog(dialogRefArray[arrayLength-1])
-}
-
+closeBtn.addEventListener("click", () => {
+    dialog.close();
+});
 
---
+++ test.js
@@ -0,0 +1,43 @@
+
+
+let dialogRefArray = ['first_dialog', 'second_dialog', 'third_dialog','fourth_dialog', 'fifth_dialog','sixth_dialog'];
+currentIndex = 0;
+
+
+function closeAllDialogs() {
+    dialogRefArray.forEach(id => {
+        const dlg = document.getElementById(id);
+        if (dlg.open) dlg.close();
+    });
+}
+
+
+function openDialog(id) {
+    currentIndex = dialogRefArray.indexOf(id);
+    closeAllDialogs();
+    const dlg = document.getElementById(id);
+    dlg.showModal();
+}
+
+
+
+
+let arrayLength = dialogRefArray.length ;
+
+
+function nextDialog(){
+    if (currentIndex < arrayLength -1) {
+        currentIndex++;
+        openDialog(dialogRefArray[currentIndex]);
+    }else openDialog(dialogRefArray[0])
+}
+
+
+function prevDialog() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        openDialog(dialogRefArray[currentIndex]);
+    }else openDialog(dialogRefArray[arrayLength-1])
+}
+
+
